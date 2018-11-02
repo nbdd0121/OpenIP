36ee7b1486403988d8dfabdb147dd9c98f520ead
Gary Guo <gary@garyguo.net>
2018-11-02 00:39:44 +0000

    Stream: Add a dummy master for AXI-Stream.

---
+++ stream/dummy_master.sv
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2018, Gary Guo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+// A dummy AXI-Stream master device that never sends words. Useful for prototyping.
+module stream_dummy_master (
+    stream_channel.master slave
+);
+
+    assign slave.t_id    = '0;
+    assign slave.t_dest  = '0;
+    assign slave.t_data  = '0;
+    assign slave.t_strb  = '0;
+    assign slave.t_keep  = '0;
+    assign slave.t_last  = 1'b0;
+    assign slave.t_user  = '0;
+    assign slave.t_valid = 1'b0;
+
+endmodule
